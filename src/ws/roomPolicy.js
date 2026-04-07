579d2cb462b863cc9c4e3c6368d985ac8910c5b6
Stevo Nagy <34813106+stevonagy@users.noreply.github.com>
2026-04-07 20:30:28 +0200

    Delete src directory

--- src/ws/roomPolicy.js
+++
@@ -1,8 +0,0 @@
-// Whitelist za kreiranje novih soba
-module.exports = {
-  MASTER_PUBLIC_KEY: "BC1YLhUQS3L6kn7biWxa3LCXe1KmzGDro6TFo3Jro2Bi3n31EKZyUoJ",
-  ALLOWED_CREATORS: [
-    "BC1YLhUQS3L6kn7biWxa3LCXe1KmzGDro6TFo3Jro2Bi3n31EKZyUoJ",
-    "BC1YLgjmxcCff55PsmYBphBrriDmeBMvru6uRdTVnzdb1BPZQiVxgPB"
-  ]
-};
